93ef62be404e8284436d04c0f58e6878d1cce43a
Chinmay-gi <chinmayihare@gmail.com>
2026-02-18 23:29:46 +0530

    Updated Server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const {connectToDB} = require("./src/config/db")
 const PORT = process.env.PORT || 7310;
 connectToDB();
 
+app.get("/",(req,res) => {
+    res.redirect("/api/home");
+})
 app.listen(PORT, () => {
     console.log("Server Started");
 });
