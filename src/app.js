da90836c527e5c39c4f84d3b418c712a891afb46
Chinmay-gi <chinmayihare@gmail.com>
2026-03-02 18:52:57 +0530

    Fixed Bug

--- src/app.js
+++ src/app.js
@@ -1,49 +1,43 @@
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
+const path = require('path');
+const cookieParser = require('cookie-parser')
 const session = require("express-session");
 const flash = require("connect-flash");
 
-const authRouter = require("./routes/auth.routes");
-const accountRouter = require("./routes/account.routes");
-const transactionRouter = require("./routes/transaction.routes");
-const pageRouter = require("./routes/page.routes");
+const authRouter = require('./routes/auth.routes');
+const accountRouter = require('./routes/account.routes');
+const transactionRouter = require('./routes/transaction.routes');
+const pageRouter = require('./routes/page.routes');
 const { authMiddleware } = require("./middleware/auth.middleware");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
+app.use(session({ secret: "secret", resave:false, saveUninitialized:true }));
 app.use(flash());
 
-app.use((req, res, next) => {
+
+app.use((req,res,next) => {
   res.locals.error = req.flash("error");
+  next();
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/account", accountRouter);
+app.use("/api/transaction", transactionRouter);
+app.use("/api", pageRouter);
+
+app.use(authMiddleware);
+app.use((req,res,next) => {
+  res.locals.user = req.user || null;
   next();
 });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
-
-app.use("/api/auth", authRouter);
-app.use("/api/account", authMiddleware, accountRouter);
-app.use("/api/transaction", authMiddleware, transactionRouter);
-app.use("/api", authMiddleware, pageRouter);
-
-app.use((req, res, next) => {
-  res.locals.user = req.user || null;
-  next();
-});
 
 
 module.exports = app;
